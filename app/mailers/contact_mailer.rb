db570e6a3dfa2e3187c25f9bc29cbf9b2aee6473
Seth Strouf <seth.strouf@doane.edu>
2021-10-24 16:38:41 -0500

    Add mailer view

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,9 +1,7 @@
 class ContactMailer < ApplicationMailer
-  default from: "contact@hannahbauer.me"
 
   def general_message(contact)
     @contact = contact
-    mail( :to => "seth.strouf@doane.edu",
-          :subject => "Message from your website!")
+    mail to: "seth.strouf@doane.edu", subject: "Message from your website!"
   end
 end
