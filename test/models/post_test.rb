d5c73e08355f2acb011ab18b86c5a168b5716ed1
Seth Strouf <seth.strouf@doane.edu>
2021-10-24 10:17:16 -0500

    Validate post model

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -2,11 +2,25 @@ require "test_helper"
 
 class PostTest < ActiveSupport::TestCase
   
-  #test "blank title should be invalid" do 
-   # post = Post.create(title: "",
-    #                   summary: "Summary here",
-     #                  body: "Body here")
-    
-  #end
+  test "title cannot be blank" do 
+    post = Post.create(title: "",
+                       summary: "Summary here",
+                       body: "Body here")
+    assert post.invalid?
+  end
+
+  test "summary cannot be blank" do 
+    post = Post.create(title: "Title here",
+                       summary: "",
+                       body: "Body here")
+    assert post.invalid?
+  end
+
+  test "body cannot be blank" do 
+    post = Post.create(title: "Title here",
+                       summary: "Summary here",
+                       body: "")
+    assert post.invalid?
+  end
 
 end
