dcbd4d2c086491dbaf918368d7382af762218418
Seth Strouf <seth.strouf@doane.edu>
2021-10-24 16:06:54 -0500

    Fix production.rb errors

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -74,9 +74,9 @@ Rails.application.configure do
     :address => 'smtp.sendgrid.net',
     :port => '587',
     :authentication => :plain,
-    :user_name => 'apikey'
+    :user_name => 'apikey',
     :password => ENV['SENDGRID_API_KEY'],
-    :domain => 'heroku.com'
+    :domain => 'heroku.com',
     :enable_starttls_auto => true
   }
 
