786d27439ee1a28864b55e39cd4235f5c73109a1
Seth Strouf <seth.strouf@doane.edu>
2021-10-26 06:30:28 -0500

    Test admin model

--- test/models/admin_test.rb
+++ test/models/admin_test.rb
@@ -1,7 +1,33 @@
 require "test_helper"
 
 class AdminTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  test "admin email should be present" do
+    admin = Admin.create(email: "",
+                         password: "foobar",
+                         password_confirmation: "foobar")
+    assert admin.invalid?
+  end
+
+  test "admin password should be present" do
+    admin = Admin.create(email: "email@example.com",
+                         password: "",
+                         password_confirmation: "")
+    assert admin.invalid?
+  end
+
+  test "email must be valid" do
+    admin = Admin.create(email: "examplemail",
+                             password: "foobar",
+                             password_confirmation: "foobar")
+    assert admin.invalid?
+  end
+
+  test "email should be downcased" do
+    admin = Admin.create(email: "EMAIL@MAIL.COM",
+                         password: "foobar",
+                         password_confirmation: "foobar")
+    assert_equal "email@mail.com", admin.email 
+  end
+
 end
