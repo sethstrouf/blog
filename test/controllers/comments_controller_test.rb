d38659a101d6b9a44b65bbf52d92536b481462b8
Seth Strouf <seth.strouf@doane.edu>
2021-10-27 20:17:38 -0500

    Finish comments function

--- test/controllers/comments_controller_test.rb
+++ test/controllers/comments_controller_test.rb
@@ -2,8 +2,4 @@ require "test_helper"
 
 class CommentsControllerTest < ActionDispatch::IntegrationTest
   
-  test "get comments show path" do
-    get comment_path(comments(:has_name))
-    assert :success
-  end
 end
