0b43d0c84e06a3817d6ee44c63415f159866d2ab
Seth Strouf <seth.strouf@doane.edu>
2021-10-31 20:22:15 -0500

    Finish image functionality, validations, and styling

--- Gemfile
+++ Gemfile
@@ -20,6 +20,9 @@ gem 'jbuilder', '~> 2.7'
 # Use Active Model has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 gem 'devise'
+gem 'active_storage_validations', '0.8.9'
+gem 'image_processing',           '1.9.3'
+gem 'mini_magick',                '4.9.5'
 
 # Use Active Storage variant
 # gem 'image_processing', '~> 1.2'
--- app/models/post.rb
+++ app/models/post.rb
@@ -5,4 +5,13 @@ class Post < ApplicationRecord
   validates :title, presence: true
   validates :summary, presence: true
   validates :body, presence: true
+  validates :image,   content_type: { in: %w[image/jpeg image/gif image/png],
+                      message: "must be a valid image format (jpeg, gif, or png)" },
+                      size:         { less_than: 5.megabytes,
+                      message:   "must be less than 5MB" }
+
+  # Returns resized image for display
+  def display_image
+    image.variant(resize_to_limit: [500,500])
+  end
 end
