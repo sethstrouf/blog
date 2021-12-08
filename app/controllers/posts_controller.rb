d69f8bd5179b496f7e972785285805e173e8950b
Seth Strouf <seth.strouf@doane.edu>
2021-12-08 17:14:49 -0600

    Change pagination to 10 per page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,7 +4,7 @@ class PostsController < ApplicationController
 
   # GET /posts or /posts.json
   def index
-    @posts = Post.paginate(page: params[:page], per_page: 5)
+    @posts = Post.paginate(page: params[:page], per_page: 10)
   end
 
   # GET /posts/1 or /posts/1.json
