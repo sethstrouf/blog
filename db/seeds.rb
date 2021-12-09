532cc057245f88c81055a117b0a22fceaaa6a56d
Seth Strouf <seth.strouf@doane.edu>
2021-12-09 06:35:18 -0600

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,5 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
 
 # Faker code used in development and initial deployment
 =begin
@@ -27,7 +21,6 @@ if Post.count.zero?
   end
 end
 =end
-
 
 # Data scraped from self-written web scraper (included in this project under 'scraper' directory)
 posts = 
@@ -82,7 +75,7 @@ posts =
 
 i = 0
 
-#if Post.count.zero?
+if Post.count.zero?
   posts.each do |post|
 
     new_post = Post.create(
@@ -100,4 +93,4 @@ i = 0
       i = i + 1
     end
   end
-#end
+end
